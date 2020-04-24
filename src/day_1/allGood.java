5226e470693c7d8b413e7b9d566f825dbf3848e4
faatihyucel <faatihyucel@gmail.comgit config --global user.password Fy2701144>
2020-04-24 16:02:35 -0400

    first commit

---
+++ src/day_1/allGood.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class allGood {
+    public static void main(String[] args) {
+
+        System.out.println("I hope everything is GOOD");
+    }
+
+}
---
+++ src/day_1/tryTryTry.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class tryTryTry {
+    public static void main(String[] args) {
+        System.out.println("tyr yr yry yryry ryryy yryr");
+    }
+}
